d88d475e9dce741828e514000ec30cb684925972
root <root@richard>
2020-03-26 19:31:57 +0700

    Update

---
+++ 02-React-JS-Tutorial/01-basic/src/contaners/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Home from '../../components/home/Home';
+
+export default class HomePage extends Component {
+
+    render() {
+        return (
+            <div>
+                <Home />
+            </div>
+        )
+    }
+}
