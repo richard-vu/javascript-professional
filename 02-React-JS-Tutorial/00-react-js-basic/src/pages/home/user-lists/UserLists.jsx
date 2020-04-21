2e84c0a917de6f6c647ce082616e341b9bca5f9b
root <root@richard>
2020-04-21 14:00:15 +0700

    Update state

---
+++ 02-React-JS-Tutorial/00-react-js-basic/src/pages/home/user-lists/UserLists.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class UserLists extends Component {
+  static propTypes = {
+    prop: PropTypes,
+  };
+
+  render() {
+    return (
+      <div className="table-responsive">
+        <table class="table">
+          <caption>List of users</caption>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
